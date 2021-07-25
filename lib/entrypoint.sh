287ad7f37600f6dfdf8439dcfab8c6c1385195d4
Justin Roberson <jroberson@aquent.com>
2021-07-25 18:01:15 -0400

    Update maintainer, Ruby version, lib versions

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-gem install rubocop -v 0.80.1
-gem install rubocop-rspec -v 1.38.1
-gem install rubocop-performance -v 1.5.2
-gem install rubocop-rails -v 2.4.2
+gem install rubocop -v 1.18.4
+gem install rubocop-rspec -v 2.4.0
+gem install rubocop-performance -v 1.11.4
+gem install rubocop-rails -v 2.11.3
 
 ruby /action/lib/index.rb
